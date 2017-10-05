50869777d445a0d7ade31e30fa2e6eefd2073193
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-10-05 14:22:51 +1000

    Updated to version 0.7.6.3

--- peek_plugin_generic_diagram_menu/__init__.py
+++ peek_plugin_generic_diagram_menu/__init__.py
@@ -3,7 +3,7 @@ from peek_plugin_base.server.PluginServerEntryHookABC import PluginServerEntryHo
 from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHookABC
 from typing import Type
 
-__version__ = '0.0.1'
+__version__ = '0.7.6.3'
 
 
 def peekServerEntryHook() -> Type[PluginServerEntryHookABC]:
