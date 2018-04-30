9e20ec122fd235455b33c8c8229a6f1f0fff0ace
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-04-30 23:38:36 +1000

    Updated for compatibility with new PayloadEnvelope Vortex update
    ORMOB-614

--- peek_plugin_generic_diagram_menu/_private/server/tuple_providers/GenericDiagramMenuTupleProvider.py
+++ peek_plugin_generic_diagram_menu/_private/server/tuple_providers/GenericDiagramMenuTupleProvider.py
@@ -1,7 +1,6 @@
 from typing import Union
 
 from twisted.internet.defer import Deferred
-from txhttputil.util.DeferUtil import deferToThreadWrap
 from vortex.Payload import Payload
 from vortex.TupleSelector import TupleSelector
 from vortex.handler.TupleDataObservableHandler import TuplesProviderABC
@@ -9,12 +8,16 @@ from vortex.handler.TupleDataObservableHandler import TuplesProviderABC
 from peek_plugin_generic_diagram_menu._private.storage.GenericDiagramMenuTuple import \
     GenericDiagramMenuTuple
 
+import logging
+from vortex.DeferUtil import deferToThreadWrapWithLogger
+
+logger = logging.getLogger(__name__)
 
 class GenericDiagramMenuTupleProvider(TuplesProviderABC):
     def __init__(self, ormSessionCreator):
         self._ormSessionCreator = ormSessionCreator
 
-    @deferToThreadWrap
+    @deferToThreadWrapWithLogger(logger)
     def makeVortexMsg(self, filt: dict,
                       tupleSelector: TupleSelector) -> Union[Deferred, bytes]:
 
@@ -23,7 +26,7 @@ class GenericDiagramMenuTupleProvider(TuplesProviderABC):
             tuples = session.query(GenericDiagramMenuTuple).all()
 
             # Create the vortex message
-            return Payload(filt, tuples=tuples).toVortexMsg()
+            return Payload(filt, tuples=tuples).makePayloadEnvelope().toVortexMsg()
 
         finally:
             session.close()
