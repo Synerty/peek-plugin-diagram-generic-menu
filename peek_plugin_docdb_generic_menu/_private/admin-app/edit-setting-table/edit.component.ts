983d93b26a470a0c458a2c7116a96f7991186fda
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-09-15 09:22:51 +1000

    PEEK-671 Refactor ComponentLifeCycleEventEmitter class

--- peek_plugin_docdb_generic_menu/_private/admin-app/edit-setting-table/edit.component.ts
+++ peek_plugin_docdb_generic_menu/_private/admin-app/edit-setting-table/edit.component.ts
@@ -1,49 +1,43 @@
-import {Component} from "@angular/core";
-import { BalloonMsgService } from "@synerty/peek-plugin-base-js"
-import {
-    ComponentLifecycleEventEmitter,
-    extend,
-    TupleLoader,
-    VortexService
-} from "@synerty/vortexjs";
-import {SettingPropertyTuple, docDbGenericMenuFilt} from "@peek/peek_plugin_docdb_generic_menu/_private";
-
+import { Component } from "@angular/core"
+import { BalloonMsgService, NgLifeCycleEvents } from "@synerty/peek-plugin-base-js"
+import { extend, TupleLoader, VortexService } from "@synerty/vortexjs"
+import { docDbGenericMenuFilt, SettingPropertyTuple } from "@peek/peek_plugin_docdb_generic_menu/_private"
 
 @Component({
-    selector: 'pl-docdb-generic-menu-edit-setting',
-    templateUrl: './edit.component.html'
+    selector: "pl-docdb-generic-menu-edit-setting",
+    templateUrl: "./edit.component.html"
 })
-export class EditSettingComponent extends ComponentLifecycleEventEmitter {
+export class EditSettingComponent extends NgLifeCycleEvents {
+    items: SettingPropertyTuple[] = []
+    loader: TupleLoader
     // This must match the dict defined in the admin_backend handler
     private readonly filt = {
         "key": "admin.Edit.SettingProperty"
-    };
-
-    items: SettingPropertyTuple[] = [];
-
-    loader: TupleLoader;
-
-    constructor(private balloonMsg: BalloonMsgService,
-                vortexService: VortexService) {
-        super();
-
-        this.loader = vortexService.createTupleLoader(this,
-            () => extend({}, this.filt, docDbGenericMenuFilt));
-
-        this.loader.observable
-            .subscribe((tuples:SettingPropertyTuple[]) => this.items = tuples);
     }
-
+    
+    constructor(
+        private balloonMsg: BalloonMsgService,
+        vortexService: VortexService
+    ) {
+        super()
+        
+        this.loader = vortexService.createTupleLoader(this,
+            () => extend({}, this.filt, docDbGenericMenuFilt))
+        
+        this.loader.observable
+            .subscribe((tuples: SettingPropertyTuple[]) => this.items = tuples)
+    }
+    
     saveClicked() {
         this.loader.save()
             .then(() => this.balloonMsg.showSuccess("Save Successful"))
-            .catch(e => this.balloonMsg.showError(e));
+            .catch(e => this.balloonMsg.showError(e))
     }
-
+    
     resetClicked() {
         this.loader.load()
             .then(() => this.balloonMsg.showSuccess("Reset Successful"))
-            .catch(e => this.balloonMsg.showError(e));
+            .catch(e => this.balloonMsg.showError(e))
     }
-
+    
 }
--- peek_plugin_docdb_generic_menu/plugin-module/_private/services/PrivateGenericMenuService.ts
+++ peek_plugin_docdb_generic_menu/plugin-module/_private/services/PrivateGenericMenuService.ts
@@ -1,13 +1,9 @@
-import {Injectable} from "@angular/core";
-
-import {
-    DocDbPopupContextI,
-    DocDbPopupService,
-    DocDbPopupTypeE
-} from "@peek/peek_plugin_docdb";
-import {PrivateGenericTupleService} from "./PrivateGenericTupleService";
-import {DocDbGenericMenuTuple} from "../tuples/DocDbGenericMenuTuple";
-import {ComponentLifecycleEventEmitter, TupleSelector} from "@synerty/vortexjs";
+import { Injectable } from "@angular/core"
+import { DocDbPopupContextI, DocDbPopupService, DocDbPopupTypeE } from "@peek/peek_plugin_docdb"
+import { PrivateGenericTupleService } from "./PrivateGenericTupleService"
+import { DocDbGenericMenuTuple } from "../tuples/DocDbGenericMenuTuple"
+import { TupleSelector } from "@synerty/vortexjs"
+import { NgLifeCycleEvents } from "@synerty/peek-plugin-base-js"
 
 /** DMS Object Item Popup Service
  *
@@ -17,90 +13,91 @@ import {ComponentLifecycleEventEmitter, TupleSelector} from "@synerty/vortexjs";
  *
  */
 @Injectable()
-export class PrivateGenericMenuService extends ComponentLifecycleEventEmitter {
-
-    private menus: DocDbGenericMenuTuple [] = [];
-
-    private readonly PLACEHOLDER_REGEXP = /\{([A-Za-z0-9_. ]+)\}/gi;
-
-    constructor(private objectPopupService: DocDbPopupService,
-                private tupleService: PrivateGenericTupleService) {
-        super();
-
+export class PrivateGenericMenuService extends NgLifeCycleEvents {
+    private menus: DocDbGenericMenuTuple [] = []
+    private readonly PLACEHOLDER_REGEXP = /\{([A-Za-z0-9_. ]+)\}/gi
+    
+    constructor(
+        private objectPopupService: DocDbPopupService,
+        private tupleService: PrivateGenericTupleService
+    ) {
+        super()
+        
         this.tupleService.tupleDataOfflineObserver
             .subscribeToTupleSelector(new TupleSelector(DocDbGenericMenuTuple.tupleName, {}))
-            .subscribe((tuples: DocDbGenericMenuTuple[]) => this.menus = tuples);
-
+            .subscribe((tuples: DocDbGenericMenuTuple[]) => this.menus = tuples)
+        
         this.objectPopupService
             .popupObservable(DocDbPopupTypeE.summaryPopup)
             .takeUntil(this.onDestroyEvent)
-            .subscribe((c: DocDbPopupContextI) => this.handlePopup(c));
-
+            .subscribe((c: DocDbPopupContextI) => this.handlePopup(c))
+        
         this.objectPopupService
             .popupObservable(DocDbPopupTypeE.detailPopup)
             .takeUntil(this.onDestroyEvent)
-            .subscribe((c: DocDbPopupContextI) => this.handlePopup(c));
-
+            .subscribe((c: DocDbPopupContextI) => this.handlePopup(c))
+        
     }
-
+    
     private handlePopup(context: DocDbPopupContextI): void {
         if (context.key == null)
-            return;
-
-        const doc = (context.document == null ? null : context.document.document) || {};
-
+            return
+        
+        const doc = (context.document == null ? null : context.document.document) || {}
+        
         for (const menu of this.menus) {
             if (!(context.modelSetKey == menu.modelSetKey || menu.modelSetKey == null))
-                continue;
-
+                continue
+            
             if (menu.url == null || menu.url.length == 0)
-                continue;
-
+                continue
+            
             // Get the two values we need to work with
-            let url = menu.url;
-            let condition = menu.showCondition == null ? '' : menu.showCondition;
-
+            let url = menu.url
+            let condition = menu.showCondition == null ? "" : menu.showCondition
+            
             if (condition.length != 0) {
-                if (condition.indexOf('==') == -1 && condition.indexOf('!=') == -1) {
-                    console.log("ERROR: There is no == or != in menu condition, skipping menu");
-                    continue;
+                if (condition.indexOf("==") == -1 && condition.indexOf("!=") == -1) {
+                    console.log("ERROR: There is no == or != in menu condition, skipping menu")
+                    continue
                 }
             }
-
+            
             // Make note of all the parameter lists, and replace all the parameters
             // with lower case.
-            let paramList: { [key: string]: true } = {};
-            url = this.replaceParams(url, paramList);
-            condition = this.replaceParams(condition, paramList);
-
-            const keys = Object.keys(doc);
+            let paramList: { [key: string]: true } = {}
+            url = this.replaceParams(url, paramList)
+            condition = this.replaceParams(condition, paramList)
+            
+            const keys = Object.keys(doc)
             for (let key of keys) {
-                let val = doc[key] == null ? '' : doc[key];
-                key = key.toLowerCase();
-                url = url.replace(`{${key}}`, val);
-                condition = condition.replace(`{${key}}`, val);
-                delete paramList[key];
+                let val = doc[key] == null ? "" : doc[key]
+                key = key.toLowerCase()
+                url = url.replace(`{${key}}`, val)
+                condition = condition.replace(`{${key}}`, val)
+                delete paramList[key]
             }
-
+            
             if (Object.keys(paramList).length != 0) {
                 // Not all parameters were filled, so don't show the menu
-                continue;
+                continue
             }
-
+            
             if (condition.length != 0) {
-                condition = condition.toLowerCase();
-                if (condition.indexOf('==') != -1) {
-                    const parts = condition.split('==');
-                    if (parts[0].trim() != (parts[1] == null ? '' : parts[1].trim()))
-                        continue;
-
-                } else if (condition.indexOf('!=') != -1) {
-                    const parts = condition.split('!=');
-                    if (parts[0].trim() == (parts[1] == null ? '' : parts[1].trim()))
-                        continue;
+                condition = condition.toLowerCase()
+                if (condition.indexOf("==") != -1) {
+                    const parts = condition.split("==")
+                    if (parts[0].trim() != (parts[1] == null ? "" : parts[1].trim()))
+                        continue
+                    
+                }
+                else if (condition.indexOf("!=") != -1) {
+                    const parts = condition.split("!=")
+                    if (parts[0].trim() == (parts[1] == null ? "" : parts[1].trim()))
+                        continue
                 }
             }
-
+            
             context.addAction({
                 name: menu.title,
                 tooltip: menu.tooltip,
@@ -108,24 +105,31 @@ export class PrivateGenericMenuService extends ComponentLifecycleEventEmitter {
                 callback: () => this.menuClicked(menu, url),
                 children: [],
                 closeOnCallback: true
-            });
+            })
         }
     }
-
-    private replaceParams(thing, paramList: { [p: string]: true }) {
-        let matches = thing.match(this.PLACEHOLDER_REGEXP);
+    
+    private replaceParams(
+        thing,
+        paramList: { [p: string]: true }
+    ) {
+        let matches = thing.match(this.PLACEHOLDER_REGEXP)
         if (matches != null) {
             for (let param of matches) {
-                const lower = param.toLowerCase();
-                thing = thing.replace(param, lower);
-                paramList[lower.replace('{', '').replace('}', '')] = true;
+                const lower = param.toLowerCase()
+                thing = thing.replace(param, lower)
+                paramList[lower.replace("{", "")
+                    .replace("}", "")] = true
             }
         }
-        return thing;
+        return thing
     }
-
-    private menuClicked(menu: DocDbGenericMenuTuple, url: string): void {
-        window.open(url);
+    
+    private menuClicked(
+        menu: DocDbGenericMenuTuple,
+        url: string
+    ): void {
+        window.open(url)
     }
-
+    
 }
