2e6928289d2d76ff3595836c313342b73ee75680
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-10-29 16:31:39 +1000

    PEEK-708 Renamed all peek services and pof/pons

--- peek_plugin_docdb_generic_menu/plugin-module/_private/services/PrivateGenericMenuService.ts
+++ peek_plugin_docdb_generic_menu/plugin-module/_private/services/PrivateGenericMenuService.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core"
-import { DocDbPopupContextI, DocDbPopupService, DocDbPopupTypeE } from "@peek/peek_plugin_docdb"
+import { DocDbPopupContextI, DocDbPopupService, DocDbPopupTypeE } from "@peek/peek_core_docdb"
 import { PrivateGenericTupleService } from "./PrivateGenericTupleService"
 import { DocDbGenericMenuTuple } from "../tuples/DocDbGenericMenuTuple"
 import { TupleSelector } from "@synerty/vortexjs"
